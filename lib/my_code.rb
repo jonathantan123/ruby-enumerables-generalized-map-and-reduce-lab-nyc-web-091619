7c548f6295f1f4034ada9932a24cdd38ea0d818a
Jonathan Tan <jonathantan@brandeis.edu>
2019-08-28 17:07:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,20 @@
 # Your Code Here
+def map (array)
+  new_array = []
+  array.length.times do |x| 
+    new_array.push(yield(array[x]))
+  end 
+  new_array
+end 
+
+def reduce(array, starting_value = nil)
+  if starting_value
+    sum = starting_value
+  else 
+    sum = array[0]
+  end 
+    array.length.times do |length|
+      sum = yield(sum, array[length])
+    end 
+    sum 
+  end 
